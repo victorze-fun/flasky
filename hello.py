56cea3dff514b023d40808eea093e1d76121cf5f
victorze <victorzevallos@protonmail.com>
2020-01-08 11:30:57 -0500

    Add templates and Flask extensions

--- hello.py
+++ hello.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return 'Hello, world!!!'
+  return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+  return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('500.html'), 500
---
+++ script.js
@@ -0,0 +1,18 @@
+function name() {
+  return 'hello'
+}
+
+function foo() {
+
+}
+
+function bar() {
+  console.log('name')
+}
+
+class Person {
+  construct(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
