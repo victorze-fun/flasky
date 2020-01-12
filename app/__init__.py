2e3821ea8f9f8732db62e119d002fdc16500a63c
victorze <victorzevallos@protonmail.com>
2020-01-11 19:34:48 -0500

    Add API

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from flask_login import LoginManager
 from flask_pagedown import PageDown
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-pagedown = PageDown()
 
 
 def create_app(config_name):
@@ -30,8 +31,11 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
